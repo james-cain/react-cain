c718abba88b0ed6311e17fa19c7b847882c397b4
james-cain <m15960192140@163.com>
2018-02-09 17:27:05 +0800

    增加配置

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -35,6 +35,7 @@ module.exports = {
   entry: [
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
+    'react-hot-loader/patch',
     // Include an alternative client for WebpackDevServer. A client's job is to
     // connect to WebpackDevServer by a socket and get notified about changes.
     // When you save a file, the client will either apply hot updates (in case
@@ -149,6 +150,9 @@ module.exports = {
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
               cacheDirectory: true,
+              plugins: [
+                'react-hot-loader/babel'
+              ]
             },
           },
           // "postcss" loader applies autoprefixer to our CSS.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import FastClick from 'fastclick';
+import { Provide } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+import Router from './router';
+import store from './store/store';
+// import './index.css';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+FastClick.attach(document.body);
+
+const render = Component => {
+  ReactDom.render()
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
