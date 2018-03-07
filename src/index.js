a728f07894d14ae219dfbd5aed6983d0fbc2a157
james-cain <m15960192140@163.com>
2018-03-07 18:03:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
 import { Provide } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import Router from './router';
+import Route from './router';
 import store from './store/store';
 // import './index.css';
 // import App from './App';
@@ -12,7 +12,25 @@ import registerServiceWorker from './registerServiceWorker';
 FastClick.attach(document.body);
 
 const render = Component => {
-  ReactDom.render()
+  ReactDOM.render(
+    // 绑定redux、热加载
+    <Provide store={store}>
+      <AppContainer>
+        <Component/>
+      </AppContainer>
+    </Provide>,
+    document.getElementById('root'),
+  )
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render(Route);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./router/', () => {
+    render(Route);
+  })
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
